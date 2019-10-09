a8ef22927fb17f21ae9bbde9974770302e731b0b
AlexandreBarbillon <alexandre.barbillon@gmail.com>
2019-10-09 12:06:19 +0200

    Adding the add button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { CatCardComponent } from './cat-card/cat-card.component';
 import { LilleFooterComponent } from './lille-footer/lille-footer.component';
 import { HeaderComponent } from './header/header.component';
 import { CatCardListComponent } from './cat-card-list/cat-card-list.component';
+import { AddButtonComponent } from './add-button/add-button.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CatCardComponent, LilleFooterComponent, HeaderComponent, CatCardListComponent ],
+  declarations: [ AppComponent, HelloComponent, CatCardComponent, LilleFooterComponent, HeaderComponent, CatCardListComponent, AddButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
